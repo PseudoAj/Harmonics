4a8d12644c450493b9d0c8ae262a6dd1d0f80fd0
ajaykrishnateja <PseudoAj@users.noreply.github.com>
2014-11-23 10:48:28 -0500

    Create getResult
    gets the phenoms

---
+++ speechDemo/getResult.php
@@ -0,0 +1,16 @@
+<?php
+
+//phpinfo();
+$_REQUEST['text'];
+$ch = curl_init("http://api.corrasable.com/words/difficult");
+$fp = fopen("result.txt", "w");
+
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+curl_exec($ch);
+curl_close($ch);
+$file = file_get_contents('./result.txt', true);
+echo $file;
+fclose($fp);
+?>
