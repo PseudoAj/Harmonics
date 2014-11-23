0eebcb1d9f491b86bc5c805ec8160ced1b9053e9
Michael Schuett <michaeljs1990@gmail.com>
2014-11-23 13:33:03 -0600

    Allow for bringing back multiple words.

--- speechDemo/getResult.php
+++ speechDemo/getResult.php
@@ -1,16 +1,12 @@
 <?php
 
-//phpinfo();
 $input=$_REQUEST['text'];
-$ch = curl_init("http://api.corrasable.com/words/".$input);
-$fp = fopen("result.txt", "w");
-
-curl_setopt($ch, CURLOPT_FILE, $fp);
+$words = explode(' ', $input);
+$ch = curl_init("http://api.corrasable.com/phonemes/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "text={$input}");
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
 curl_exec($ch);
 curl_close($ch);
-$file = file_get_contents('./result.txt', true);
-echo $file;
-fclose($fp);
-?>
+
