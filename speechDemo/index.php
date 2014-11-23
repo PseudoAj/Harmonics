fefab5abbba0668f4434735e9e2fce9d6641ef05
ajaykrishnateja <PseudoAj@users.noreply.github.com>
2014-11-23 10:47:22 -0500

    Create index.php
    base version

---
+++ speechDemo/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+</head>
+<body>
+<form action="javascript:void(0);" method="post">
+    <input type="text" name="text" placeholder="enter a text" />
+    <input type="submit" value="submit" />
+</form>
+
+<script type="text/javascript">
+    $("form").submit(function(){
+        var str = $(this).serialize();
+        $.ajax('getResult.php', str, function(result){
+            alert(result); 
+        }
+        return(false);
+    });
+</script>
+</body>
+</html>
